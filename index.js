14a26d53bdeb9e3bf652c26460a1b5cc4d2754f5
dvsrshohan <shohan.ridigames@gmail.com>
2022-10-27 12:30:19 +0600

    Add Some Comments in index.js

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ app.get('/', (req, res) => {
     res.send('Online School  API Runing');
 });
 
+//Send All Courses
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+
+//Send Data For Courses CourseDetels page
 app.get('/course/:id', (req, res) => {
     const id = req.params.id;
     const selectCourse = courses.find(c => c.id == id);
     res.send(selectCourse);
 });
+
+//Send Data For CheckOut page
 app.get('/course/:id/checkout', (req, res) => {
     const id = req.params.id;
     const selectCourse = courses.find(c => c.id == id);
